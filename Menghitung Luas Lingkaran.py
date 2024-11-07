f7a9a8548c22e648b77dc99590a0304f694dfb1e
IamMuel <muelkb@gmail.com>
2024-11-07 13:34:04 +0700

    first commit

---
+++ Menghitung Luas Lingkaran.py
@@ -0,0 +1,14 @@
+# Deklarasi nilai jari-jari lingkaran
+jarijari = int(input("Masukan jari-jari Lingkaran = "))
+
+# Deklarasi hasil luas lingkaran
+phi = 3.14
+luas = phi * jarijari ** 2
+
+# Menampilkan rumus, nilai jari-jari, nilai π, dan hasil luas lingkaran
+print(f"Jari-jari lingkaran adalah: {jarijari}")
+print(f"Nilai π (phi) adalah: {phi}")
+print("Hasil dari Luas Lingkaran adalah: π x r^2")
+print(f"= {phi} x {jarijari}^2")
+print(f"= {phi} x {jarijari**2}")
+print(f"= {luas}")
